372cf38e8ef9bad9802f710174e6e4617c9d5677
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-25 20:45:17 +0100

    Set LD path

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
 export CABAL_DIR="$PREFIX"
+export LD=x86_64-conda_cos6-linux-gnu-ld
 ghc-pkg recache
 cabal update
 cabal install --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --ghc-options="-threaded" --extra-lib-dirs=$PREFIX/lib --extra-include-dirs=$PREFIX/include happy-1.19.9
