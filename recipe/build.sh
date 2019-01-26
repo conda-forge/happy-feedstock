c13641c72b1e6ff5b86683ed69e1c7dca04f1f2a
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-26 09:21:02 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,9 @@ export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
 export CABAL_DIR="$PREFIX"
 export LDFLAGS=" -lgmp $LDFLAGS $LDFLAGS"
 ghc-pkg recache
+ghc-pkg describe rts
+ghc-pkg describe rts > rts.pkg
+perl -pi -e 's/$PREFIX\/lib\/ghc-8.2.2\/rts/$PREFIX\/lib\/ghc-8.2.2\/rts \$\{pkgroot\}\/../g' rts.pkg
+cat rts.pkg
 cabal update
 cabal install --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --ghc-options="-threaded -lgmp -threaded -pgmc x86_64-conda_cos6-linux-gnu-cc -pgml x86_64-conda_cos6-linux-gnu-cc" --extra-lib-dirs=$PREFIX/lib --extra-include-dirs=$PREFIX/include happy-1.19.9
